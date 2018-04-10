e4383e4e8ef9c27f8112c8d1ed4d788ec772c745
silviaesteban <32508716+silviaesteban@users.noreply.github.com>
2018-04-10 11:21:06 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,98 @@
+(function (blink) {
+	'use strict';
 
+	var PopTropicaStyle = function () {
+			blink.theme.styles.basic.apply(this, arguments);
+		},
+		page = blink.currentPage;
+
+	PopTropicaStyle.prototype = {
+		bodyClassName: 'content_type_clase_poptropica',
+		ckEditorStyles: {
+			name: 'poptropica',
+			styles: [
+
+				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title bck-title1'} },
+				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
+
+				{ name: 'Lista ordenada1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
+				{ name: 'Lista ordenada2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol2' } },
+				{ name: 'Lista ordenada3', element: 'ol', attributes: { 'class': 'bck-ol bck-ol3' } },
+				{ name: 'Lista ordenada4', element: 'ol', attributes: { 'class': 'bck-ol bck-ol4' } },
+
+				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box1' } },
+				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box2' } },
+				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box3' } },
+
+				{ name: 'Icono Draw', element: 'span', attributes: { 'class': 'icon icon-draw' } },
+				{ name: 'Icono Write', element: 'span', attributes: { 'class': 'icon icon-write' } },
+				{ name: 'Icono Speaking', element: 'span', attributes: { 'class': 'icon icon-speaking' } },
+				{ name: 'Icono Reading', element: 'span', attributes: { 'class': 'icon icon-reading' } },
+				{ name: 'Icono Star', element: 'span', attributes: { 'class': 'icon icon-star' } }
+			]
+		},
+
+		init: function () {
+			var parent = blink.theme.styles.basic.prototype;
+			parent.init.call(this);
+			this.addActivityTitle();
+			if(window.esWeb) return;
+			this.removeFinalSlide();
+			this.handleScrollEnd();
+			this.setTooltip();
+			window.editar && this.configEditor();
+
+			if ($('.navbar-bottom').length > 0) {
+ 				$('.navbar-bottom ol').wrapAll('<div id="bottom-navigator"></div>');
+		 		var width = 0;
+		 		$('.navbar-bottom li').each(function(i, elem){ width += $(elem).outerWidth(true); });
+		 		$('.navbar-bottom ol').css('width', width * 1.1);
+		 		var scroll = new IScroll('#bottom-navigator', {
+		 			scrollX: true,
+		 			scrollY: false,
+		 			eventPassthrough: true
+		 		});
+		 		scroll.on('scrollEnd', this.handleScrollEnd);
+		 		this.handleScrollEnd.call(scroll);
+	 		}
+
+		},
+
+		configEditor: function (editor) {
+			editor.dtd.$removeEmpty['span'] = false;
+		},
+
+
+		addActivityTitle: function () {
+			if (!blink.courseInfo || !blink.courseInfo.unit) return;
+			$('.libro-left').find('.title').html(function () {
+				return $(this).html().trim() + ' > ' + blink.courseInfo.unit;
+			});
+		},
+
+		handleScrollEnd: function () {
+ 			$('#bottom-navigator')
+ 				.removeClass('show_left')
+ 				.removeClass('show_right');
+
+ 			if (this.x < 0) {
+ 				$('#bottom-navigator').addClass('show_left');
+ 			}
+ 			if (this.x > this.maxScrollX) {
+ 				$('#bottom-navigator').addClass('show_right');
+ 			}
+
+ 		},
+
+		setTooltip: function () {},
+
+		getEditorStyles: function () {
+			return this.ckEditorStyles;
+		}
+	};
+
+	PopTropicaStyle.prototype = _.extend({}, new blink.theme.styles.basic(), PopTropicaStyle.prototype);
+
+	blink.theme.styles.poptropica = PopTropicaStyle;
+
+})( blink );
